1a5d97e5beb5f2c46c3af15927e0ed00580da375
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-24 11:32:12 -0700

    copied

---
+++ index.js
@@ -0,0 +1,45 @@
+const uuidV4 = require('uuid/v4');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" });
+
+exports.handler = (event, context, callback) => {
+  console.log("Processing...");
+  const params = {
+    Item: {
+      user_id: uuidV4(),
+      email: event.email,
+      password: event.password
+    },
+    TableName: "USER_INFO"
+  };
+
+
+  var response;
+
+  console.log("HERE")
+  docClient.put(params, function (err, data) {
+    if (err) {
+
+      response = {
+        statusCode: 403,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify('ERROR, email and/or password not allowed.'),
+      };
+    } else {
+      console.log("RESPONSE", response)
+      response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify('Successful creation in table.'),
+      };
+    }
+  })
+
+  return response;
+};
