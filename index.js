2be85d431f0ff6609c7282a0be8007361459bd35
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-24 12:51:48 -0700

    final update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" });
 
 exports.handler = (event, context, callback) => {
-  console.log("Processing...nowssss");
   const params = {
     Item: {
       user_id: uuidV4(),
